47d984b89e8649fa2966a2c5d3396f7a9984752e
SealtielFreak <sealtielfreak@yandex.com>
2026-02-04 21:18:27 -0600

    Minor changes

--- examples/d_ntree.py
+++ examples/d_ntree.py
@@ -1,24 +1,23 @@
 import random
 
+import numpy as np
+
 from ktree.ntree import NTreeDynamic
 
 N_DIMENSION = 3
 
 random.seed(0)
 
-tree = NTreeDynamic(1)
+tree = NTreeDynamic(2)
+data = np.random.uniform(0, 64, (32 * 32, 3))
 
-for _ in range(5):
-    tree.insert([random.randint(0, 64) for _ in range(N_DIMENSION)])
+for a in data:
+    tree.insert(a)
 
 sorted_data = tree.sort()
-l_sorted_data = 0
 
-for shape, data in sorted_data:
-    print("Shape:", shape)
-    print(len(data), data)
+for n, (shape, data) in enumerate(sorted_data):
+    print(f"Shape[{n}]:", shape)
+    # print(len(data), data)
 
-    l_sorted_data += len(data)
-
-print(l_sorted_data)
 print(tree.shape)
